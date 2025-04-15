a5508dfa7a864c4ee116f65689ff946b4813561f
indiebreath <indiebreath@proton.me>
2025-04-15 10:04:59 +1000

    Added start of gameplay introduction

--- Main.java
+++ Main.java
@@ -1,5 +1,47 @@
+import java.util.HashMap;
+
+/**
+ * Main.
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        HashMap<String, Boolean> items = new HashMap<String, Boolean>();
+        items.put("lantern", false);
+        items.put("oil", false);
+        items.put("lighter", false);
+
+        // a1 is top-left, e5 is bottom-right. Letters are horizontal, numbers are
+        // vertical.
+        String playerLocation = "c3";
+        boolean play = true;
+
+        // Introduction text.
+        System.out.println("""
+                You awake into darkness. Looking around, you are barely able to make
+                out the shape of a room around you. There appears to be a door to the north, another
+                one to the south, and a table in the middle of the room, which contains a lantern on
+                top of it.\n""");
+
+        // Main game loop.
+        while (play) {
+            switch (playerLocation) {
+                case "c3":
+                    if (!items.get("lantern")) {
+                        int[] validInputs = { 1, 2, 3 };
+                        int choice = Utils.getChoice("""
+                                To both the north and south are doors, the northern one wooden, while
+                                the southern one iron. On the table is a lantern. What do you do?
+                                1) Attempt the north door.
+                                2) Attempt the south door.
+                                3) Pick up the lantern.""", validInputs);
+                        System.out.println(choice);
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+            play = false;
+        }
     }
 }
--- Utils.java
+++ Utils.java
@@ -2,9 +2,9 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 /**
- * A generic file containing functions that may be used regularly across the projected, all
- * separated into their own file in order to avoid clogging up the main class with
- * misceallaneous functions.
+ * A generic file containing functions that may be used regularly across the
+ * projected, all separated into their own file in order to avoid clogging up
+ * the main class with misceallaneous functions.
  */
 public class Utils {
     public static boolean contains(final int[] array, final int key) {
@@ -12,9 +12,10 @@ public class Utils {
     }
 
     /**
-     * Called when prompting the player for input, with the prompt variable being used to tell
-     * the player what options they have, and the validInputs variable being used to verify whether
-     * the given input is valid. Will loop until a valid input is given then return the input.
+     * Called when prompting the player for input, with the prompt variable being
+     * used to tell the player what options they have, and the validInputs variable
+     * being used to verify whether the given input is valid. Will loop until a
+     * valid input is given then return the input.
      */
     public static int getChoice(String prompt, int[] validInputs) {
         Scanner scanner = new Scanner(System.in);
